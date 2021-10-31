1974d50f7b1b8942d795c7e0090c78c1f6f57f7b
Adham Shams <adham20011@gmail.com>
2021-10-31 18:58:31 +0200

    font fix 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './app.css';
+import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomeScreen from './screens/HomeScreen.js';
