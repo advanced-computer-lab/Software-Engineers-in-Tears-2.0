dd3d7b5cc1f88da8f06db4e0037dd7e3549d9630
maiarabbas <maiarashraf123@gmail.com>
2021-11-06 10:51:39 +0200

    final createflight

--- client/src/components/CreateFlight.js
+++ client/src/components/CreateFlight.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 
 
 
+
+
+
 //import { createIndexes } from '../../../models/User.js';
 
 function CreateFlight() {
@@ -69,47 +72,47 @@ const [input, setInput]=useState({
     
     <div class="form-group">
     <label>FlightNumber</label> <br/>
-        <input type="text" value={input.FlightNumber} class="form-control" id="fnumber" onChange={create} name="FlightNumber" /> <br/>
+        <input type="text" value={input.FlightNumber} class="form-control" id="fnumber" onChange={create} name="FlightNumber" required /> <br/>
         </div>
 
         <div class="form-group">
     <label>AirportTerminal</label> <br/>
-        <input type="text" value={input.AirportTerminal} class="form-control" id="terminal" onChange={create} name="AirportTerminal" /> <br/>
+        <input type="text" value={input.AirportTerminal} class="form-control" id="terminal" onChange={create} name="AirportTerminal" required/> <br/>
         </div> 
 
         <div class="form-group">
     <label>DepartureTime</label> <br/>
-        <input type="time" value={input.DepartureTime} class="form-control" id="dtime" onChange={create} name="DepartureTime" /> <br/>
+        <input type="time" value={input.DepartureTime} class="form-control" id="dtime" onChange={create} name="DepartureTime" required/> <br/>
         </div> 
 
         <div class="form-group">
     <label>ArrivalTime</label> <br/>
-        <input type="time" value={input.ArrivalTime} class="form-control" id="atime" onChange={create} name="ArrivalTime" /> <br/>
+        <input type="time" value={input.ArrivalTime} class="form-control" id="atime" onChange={create} name="ArrivalTime" required /> <br/>
         </div>
 
         <div class="form-group">
     <label>From</label> <br/>
-        <input type="text" value={input.From} class="form-control" id="from" onChange={create} name="From" /> <br/>
+        <input type="text" value={input.From} class="form-control" id="from" onChange={create} name="From" required /> <br/>
         </div>
 
         <label>To</label> <br/>
         <div class="form-group">
-        <input type="text" value={input.To} class="form-control" id="to" onChange={create} name="To" /><br/>
+        <input type="text" value={input.To} class="form-control" id="to" onChange={create} name="To" required/><br/>
         </div>
 
        
         <label>Flight Date</label> <br/>
         <div class="form-group">
-        <input type="date"  value={input.Flight_Date} class="form-control" id="date" onChange={create} name="Flight_Date"/> <br/>
+        <input type="date"  value={input.Flight_Date} class="form-control" id="date" onChange={create} name="Flight_Date" required/> <br/>
         </div>
         
         <label>Cabin</label> <br/>
         <div class="form-group">
-        <input type="text" value={input.Cabin} class="form-control" id="cabin" onChange={create} name="Cabin"/><br/>
+        <input type="text" value={input.Cabin} class="form-control" id="cabin" onChange={create} name="Cabin" required/><br/>
         </div>
         <label>Seats Available</label> <br/>
         <div class="form-group">
-        <input type="number" value={input.Seats_Available_on_Flight} class="form-control" id="ecs" onChange={create} name="Seats_Available_on_Flight" /><br/>
+        <input type="number" value={input.Seats_Available_on_Flight} class="form-control" id="ecs" onChange={create} name="Seats_Available_on_Flight" required /><br/>
         </div>
       
       <button onClick={handle}>Create</button>
