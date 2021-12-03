13c974afffab009b4fed4b3384f372aa472f5e47
maiarabbas <maiarashraf123@gmail.com>
2021-12-03 13:23:06 +0200

    search and create flight requirements, added arrivalDate attribute

--- client/src/components/Button1.js
+++ client/src/components/Button1.js
@@ -9,9 +9,9 @@ function Button1(props) {
       onMouseEnter={() => setHover('rgba(207,117,0,1)')} 
       onMouseLeave={() => setHover('rgba(240,165,0,1)')}
       onClick={props.disabled ? null : props.onClick}
-      style={{...props.style, background: props.disabled ? 'rgba(220,220,220,1)' : hover, cursor: 'pointer', borderRadius: 10, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', boxShadow: '0px 1px 5px  0.35px #000'}}
+      style={{...props.style, background: props.disabled ? 'rgba(220,220,220,1)' : hover, cursor: props.disabled? 'default': 'pointer', borderRadius: 10, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', boxShadow: '0px 1px 5px  0.35px #000'}}
     >
-      {props.loading ? <ReactLoading type={"spin"} color={"#f4f4f4"} height={27} width={27}/> : <label style={{fontFamily: 'Archivo', fontSize: props.style.fontSize||20, color: props.style.color||'rgba(244,244,244,1)', cursor: 'pointer'}}>{props.title}</label>}
+      {props.loading ? <ReactLoading type={"spin"} color={"#f4f4f4"} height={27} width={27}/> : <label style={{fontFamily: 'Archivo', fontSize: props.style.fontSize||20, color: props.style.color||'rgba(244,244,244,1)', cursor: props.disabled? 'default': 'pointer'}}>{props.title}</label>}
     </div>
   );
 }
