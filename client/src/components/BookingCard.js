a5aed390168ab6d6aa1aaeda4f2ac887405a440e
Aya Saleh <aya_saleh2@yahoo.com>
2021-12-24 18:33:10 +0200

    -

--- client/src/components/BookingCard.js
+++ client/src/components/BookingCard.js
@@ -102,8 +102,8 @@ function BookingCard(props) {
                     <label style={{fontFamily: 'Archivo', fontSize: 18, marginTop: 20, marginLeft: 20}}>Seats Booked: {props.Booking.returnFlightSeats.length === 0 ? <label style={{color: '#F0A500'}}>No Seats Booked</label> : <label style={{color: '#F0A500'}}>{props.ReturnFlight.Cabin.substring(0,1)}{props.Booking.returnFlightSeats.join(', ' + props.ReturnFlight.Cabin.substring(0,1))}</label>}</label>
                     <label style={{fontFamily: 'Archivo', fontSize: 18, marginTop: 20, marginLeft: 20}}>Baggage Allowance: <label style={{color: '#F0A500'}}>{props.ReturnFlight.Baggage_Allowance ? props.DepartBaggageAllowance : 'N/A'} KG</label></label>
                     <div style={{display: 'flex', flexDirection: 'row', width: '100%', marginLeft: 20, marginTop: 40}}>
-                        <Button1 style={{width: 180, height: 35}} title={'Modify Flight'} onClick= {() => handleReturn()} />
-                        <Button1 style={{width: 180, height: 35, marginLeft: 20}} title={'Change Seats'} onClick={() => history.push(`/booking/${props.Booking._id}/seats/return/edit`)}/>
+                        <Button1 style={{width: 200, height: 35}} title={'Modify Reservation'} onClick= {() => handleReturn()} />
+                        <Button1 style={{width: 200, height: 35, marginLeft: 20}} title={'Change Seats'} onClick={() => history.push(`/booking/${props.Booking._id}/seats/return/edit`)}/>
                     </div>
                 </div>
             }
