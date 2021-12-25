ab9700b2b4fd5ab94e667dbc899ea85909d8fbaa
maiarabbas <maiarashraf123@gmail.com>
2021-12-25 18:12:53 +0200

    baggage

--- client/src/screens/ModifyReturnBookingResults.js
+++ client/src/screens/ModifyReturnBookingResults.js
@@ -120,7 +120,7 @@ function ModifyReturnBookingResults(props) {
                 <label style={{fontFamily: 'Archivo', fontSize:20, marginLeft:5}}>{flight.Flight_Date?(flight.Flight_Date.substring(0,7)):'N/A'}</label>
                 <label style={{fontFamily: 'Archivo', fontSize: 20, marginLeft:20}}>#{flight.FlightNumber?flight.FlightNumber:'N/A'}</label>
                 <Image src={require("../assets/images/lug5.png").default} style={{height: 30, width: 30, marginLeft: 20}}/>
-                <label style={{fontFamily: 'Archivo'}}>{flight.Baggage_Allowance?flight.Baggage_Allowance:'N/A'}</label>
+                <label style={{fontFamily: 'Archivo'}}>{flight.Baggage_Allowance?flight.Baggage_Allowance:'N/A'}Kg</label>
                 <label style={{fontFamily: 'Archivo', marginLeft:20, fontSize: 20}}><label style={{fontWeight:'bold'}}>$</label>{flight.Price?flight.Price:'N/A'}</label>
                 <label style={{fontFamily: 'Archivo', marginLeft:20, fontSize: 20}}>{flight.DepartureTime?flight.DepartureTime:'N/A'}</label>
                 <Image src={require("../assets/images/line2.png").default} style={{height: 25, width: 30, marginLeft:10}}/>
